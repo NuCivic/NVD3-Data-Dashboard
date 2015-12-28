b2226aeb85f9cff0aec881c93a04afff65badd14
Paul Walker <pjwalker76@gmail.com>
2015-12-28 20:58:38 +0000

    use query string

--- docroot/router.js
+++ docroot/router.js
@@ -1,13 +1,49 @@
+/**
+ * Backbone Router
+ *
+ * NOTE: To ignore baseURL
+ *
+ *	Backbone.history.start({
+ *			pushState: true,
+ *			root: "/public/search/"
+ *	});
+ *
+ **/
 define(['backbone'], function (Backbone) {
 	console.log('[router] loaded 11');
   var Router = Backbone.Router.extend({
 		routes : {
-			'dash/:backend/:id' : 'getDash' 
+			'dash/comp/?*queryString' : 'getCompDash' 
 		},
 
-		getDash : function (backend, id) {
-			console.log('getDash', backend, id);
-		}
+		getCompDash : function (queryString) {
+//			require(['views/dashCompViews'], function (Views) {
+				console.log(queryString);
+				var params = this.parseQueryString(queryString);
+				console.log('getDash', params);
+//			});
+		},
+		parseQueryString : function (queryString) {
+			var params = {};
+			if(queryString){
+					_.each(
+							_.map(decodeURI(queryString).split(/&/g),function(el,i){
+									var aux = el.split('='), o = {};
+									if(aux.length >= 1){
+											var val = undefined;
+											if(aux.length == 2)
+													val = aux[1];
+											o[aux[0]] = val;
+									}
+									return o;
+							}),
+							function(o){
+									_.extend(params,o);
+							}
+					);
+			}
+			return params;
+    }
 	});
 
 	var AppRouter = new Router();
