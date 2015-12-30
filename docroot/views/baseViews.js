bf73b49fec5dd9741984b1ea992124698a64aad9
Paul Walker <pjwalker76@gmail.com>
2015-12-30 18:13:22 +0000

    Templating works.

--- docroot/views/baseViews.js
+++ docroot/views/baseViews.js
@@ -4,7 +4,18 @@ define(['backbone', 'underscore'], function (Backbone, _) {
     _init : function (opts) {
       console.log('[baseView]', opts);
       _.extend(this, opts);
+    },
+
+    template: function (data) {
+      console.log('template', data, $(this.tpl).html(), _.template($(this.tpl).html(), {title : 'title'}));
+      var t = _.template($(this.tpl).html());
+      var c = t(data);
+      this.$el.html(c);
     }
+
   });
+
   return Views;
+}, function (err) {
+  console.log("[baeViews]ERR", err);
 });
